05e8a01015db906f55eef795e8481e3feb284b0b
Semi-koron <6jimoto6@gmail.com>
2024-02-05 21:34:13 +0900

    add article editer layouts

--- src/component/pages/Write/write.tsx
+++ src/component/pages/Write/write.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Header from "../../header/header";
+import Markdownform from "../../markdown/markdown";
 import ArticlesMap from "../../airticlesMap/articlesmap";
-import styles from "../../styles/top.module.css";
+import styles from "../../styles/write.module.css";
+import buttonStyles from "../../button/button.module.css";
 
 type article = {
   id: number;
@@ -14,7 +16,21 @@ export const Write = () => {
   return (
     <div>
       <Header />
-      <div className={styles.wholePage}></div>
+      <div className={styles.wholePage}>
+        <div className={styles.mainEditer}>
+          <h2>記事のタイトル</h2>
+          <input type="text" className={styles.contentTitle} />
+          <h2>記事のタグ</h2>
+          <input type="text" className={styles.contentTitle} />
+          <Markdownform />
+        </div>
+        <div className={styles.sideEditer}>
+          <button className={buttonStyles.button}>オートマークダウン</button>
+        </div>
+      </div>
+      <div>
+        <button className={buttonStyles.button}>投稿</button>
+      </div>
     </div>
   );
 };
